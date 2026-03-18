8b0ecfbb69b9c3dbf5f7414f34011cdc593e2b70
doshamine <doshamine@gmail.com>
2026-03-18 13:26:17 +0600

    feat: created models

---
+++ auth/hash.py
@@ -0,0 +1,13 @@
+import bcrypt
+
+
+def hash_password(password: str) -> str:
+    password = password.encode('utf-8')
+    hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
+    return hashed_password.decode('utf-8')
+
+
+def check_password(password: str, hashed_password: str) -> bool:
+    password = password.encode('utf-8')
+    hashed_password = hashed_password.encode('utf-8')
+    return bcrypt.checkpw(password, hashed_password)
--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    __tablename__ = 'users'
+
+    first_name = models.CharField(max_length=50, blank=False, null=False)
+    last_name = models.CharField(max_length=50, blank=False, null=False)
+    email = models.EmailField(unique=True, null=False)
+    password = models.CharField(max_length=255, blank=False, null=False)
+    is_active = models.BooleanField(null=False, default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    roles = models.ManyToManyField('Role', through='UserRoles', related_name='users')
+
+
+class Role(models.Model):
+    __tablename__ = 'roles'
+
+    name = models.CharField(max_length=50, unique=True, blank=False, null=False)
+    display_name = models.CharField(max_length=50, unique=True, blank=False, null=False)
+    description = models.TextField()
+    permissions = models.ManyToManyField('Permission', related_name='roles')
+
+
+class UserRoles(models.Model):
+    __tablename__ = 'user_roles'
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    valid_until = models.DateTimeField(null=False)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'role'],
+                name='uq_user_roles_user_role'
+            )
+        ]
+
+
+class Permission(models.Model):
+    __tablename__ = 'permissions'
+
+    name = models.CharField(max_length=100, blank=False, null=False)
+    description = models.TextField()
